d1702f195bbed75781e9cc98aa01260d5ea02af8
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2021-07-31 19:42:40 +0300

    сделал 1е задание

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+   startBtn: document.querySelector('button[data-start]'),
+   stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', startColorSwitcher);
+refs.stopBtn.addEventListener('click', stopColorSwitcer);
+
+let intervalID = null;
+
+function startColorSwitcher() {
+   refs.startBtn.disabled = true;
+   refs.stopBtn.disabled = false;
+   swithBodyBgColor();
+   intervalID = setInterval(swithBodyBgColor, 1000);
+}
+
+function stopColorSwitcer() {
+   refs.startBtn.disabled = false;
+   refs.stopBtn.disabled = true;
+   clearInterval(intervalID);
+}
+
+function swithBodyBgColor() {
+   document.body.style.backgroundColor = getRandomHexColor();
+}
+
+// функция генерации случайного цвета
+function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
