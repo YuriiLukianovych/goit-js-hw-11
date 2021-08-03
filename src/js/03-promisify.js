50c1e3d2b82237909a02065ef039bfd429303472
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2021-08-03 14:26:41 +0300

    правки промисов

--- src/js/03-promisify.js
+++ src/js/03-promisify.js
@@ -11,9 +11,9 @@ const delay = ms => {
 const logger = time => console.log(`Fulfilled after ${time}ms`);
 
 // Tests
-delay(2000).then(logger); // Fulfilled after 2000ms
-delay(1000).then(logger); // Fulfilled after 1000ms
-delay(1500).then(logger); // Fulfilled after 1500ms
+// delay(2000).then(logger); // Fulfilled after 2000ms
+// delay(1000).then(logger); // Fulfilled after 1000ms
+// delay(1500).then(logger); // Fulfilled after 1500ms
 
 // ======================= Subtask 2 =======================
 const users = [
@@ -23,12 +23,10 @@ const users = [
 ];
 
 const toggleUserState = (allUsers, username) => {
-   return new Promise(resolve => {
-      const updatedUsers = allUsers.map(user =>
-         user.name === username ? { ...user, active: !user.active } : user,
-      );
-      resolve(updatedUsers);
-   });
+   const updatedUsers = allUsers.map(user =>
+      user.name === username ? { ...user, active: !user.active } : user,
+   );
+   return Promise.resolve(updatedUsers);
 };
 
 // Currently the function works like this
@@ -74,7 +72,7 @@ const logError = id => {
 // makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
 
 // The function should work like this
-makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
-makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
-makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
-makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
+// makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+// makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+// makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+// makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
